ca1231e2cdfb22bdca44e2c8cdee6537c855d5a0
hirle <eric.hirlemann@gmail.com>
2020-05-23 12:11:00 +0200

    Coverage

---
+++ src/__tests__/ColoredLed.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ColoredLed from '../ColoredLed';
+import { create } from 'react-test-renderer';
+import util from 'util';
+
+describe('ColoredLed', () => {
+    it('should display a red indicator light', () => {
+        const mustBeRed = create(<ColoredLed color="red" />);
+        expect(mustBeRed.toJSON()).toMatchSnapshot();
+
+        const mustBeRedToo = create(<ColoredLed color="#FF0000" />);
+        const mustBeRedTooJson: any = mustBeRed.toJSON();
+        expect(mustBeRedToo.toJSON()).toMatchObject(mustBeRedTooJson);
+    });
+});
